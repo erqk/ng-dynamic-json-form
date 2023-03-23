7cf2552f35acdfbc441d1f814cb2529fada6e506
QianKai <eqiankai@hotmail.com>
2023-03-23 16:04:49 +0800

    chore: remove unecessary data type

--- src/app/core/models/json-form-control-data.model.ts
+++ src/app/core/models/json-form-control-data.model.ts
@@ -3,7 +3,6 @@ import { JsonFormGroupData } from "./json-form-group-data.model";
 export interface JsonFormControlData {
     label: string;
     value: any;
-    valueType?: string;
     validators?: string[];
     child?: JsonFormGroupData;
 }
