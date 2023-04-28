4ba9b60697d1f4b08b4724c5af225e9b921c3a71
QianKai <eqiankai@hotmail.com>
2023-04-28 20:08:35 +0800

    fix: previous styles overwritten by later styles

--- projects/ng-dynamic-json-form/src/lib/components/grid-item-wrapper/grid-item-wrapper.component.ts
+++ projects/ng-dynamic-json-form/src/lib/components/grid-item-wrapper/grid-item-wrapper.component.ts
@@ -11,8 +11,7 @@ import { FormControlConfig } from '../../models';
 })
 export class GridItemWrapperComponent {
   @Input() parentId = '';
-  @Input() data: FormControlConfig =
-    {} as FormControlConfig;
+  @Input() data: FormControlConfig = {} as FormControlConfig;
 
   get hostId(): string {
     return this.parentId
@@ -47,15 +46,16 @@ export class GridItemWrapperComponent {
 
     // Set `id` to this component so that `querySelector` can find it correctly.
     this.renderer2.setAttribute(hostElement, 'id', this.hostId);
+
+    const styles: { [key: string]: string } = {
+      ...(this.gridRow && { 'grid-row': this.gridRow }),
+      ...(this.gridColumn && { 'grid-column': this.gridColumn }),
+    };
+
     this.renderer2.setAttribute(
       hostElement,
       'style',
-      `grid-row: ${this.gridRow}`
-    );
-    this.renderer2.setAttribute(
-      hostElement,
-      'style',
-      `grid-column: ${this.gridColumn}`
+      Object.keys(styles).reduce((a, key) => `${a}${key}:${styles[key]};`, '')
     );
   }
 }
