f21fe191be38ad06c63b412ff248aea0e28d8882
QianKai <eqiankai@hotmail.com>
2023-05-02 17:23:13 +0800

    style: add padding to screen edge

--- projects/demo/src/app/pages/page-home/page-home.component.ts
+++ projects/demo/src/app/pages/page-home/page-home.component.ts
@@ -3,17 +3,23 @@ import { Component } from '@angular/core';
 import { MarkdownModule } from 'ngx-markdown';
 import { ContentWrapperComponent } from '../../shared/content-wrapper/content-wrapper.component';
 import { LanguageDataService } from '../../features/language/services/language-data.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-page-home',
   standalone: true,
-  imports: [CommonModule, MarkdownModule, ContentWrapperComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MarkdownModule,
+    ContentWrapperComponent,
+  ],
   templateUrl: './page-home.component.html',
   styleUrls: ['./page-home.component.scss'],
 })
 export class PageHomeComponent {
   language$ = this.languageDataService.language$;
   languageData$ = this.languageDataService.languageData$;
-  
+
   constructor(private languageDataService: LanguageDataService) {}
 }
