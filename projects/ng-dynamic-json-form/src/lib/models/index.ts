053844b54296149fc34be86f73f870b5d9ea0ed0
QianKai <eqiankai@hotmail.com>
2023-05-21 21:52:36 +0800

    fix(lib): export UiComponents type

--- projects/ng-dynamic-json-form/src/lib/models/index.ts
+++ projects/ng-dynamic-json-form/src/lib/models/index.ts
@@ -3,3 +3,4 @@ export * from './form-control-config.model';
 export * from './form-control-options.model';
 export * from './form-validator-config.model';
 export * from './form-control-condition.model';
+export * from './ui-components-type.model';
