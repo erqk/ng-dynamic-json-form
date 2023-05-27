3635973593c7dfe3eb39b0f26223d6c021f220e6
QianKai <eqiankai@hotmail.com>
2023-05-27 18:42:04 +0800

    feat(docs): save current document version

--- src/app/features/document/services/document-version.service.ts
+++ src/app/features/document/services/document-version.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -7,5 +7,20 @@ import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
 export class DocumentVersionService {
   versions = ['3', '2', '1'];
 
-  currentVersion$ = new BehaviorSubject<string>(this.versions[0]);
+  private _currentVersion$ = new BehaviorSubject<string>(this.versions[2]);
+
+  setVersion(version: string): void {
+    if (!this.versions.includes(version)) return;
+
+    this._currentVersion$.next(version);
+    window.localStorage.setItem('docs-version', version);
+  }
+
+  get currentVersion$(): Observable<string> {
+    return this._currentVersion$.asObservable();
+  }
+
+  get currentVersion(): string {
+    return this._currentVersion$.value;
+  }
 }
