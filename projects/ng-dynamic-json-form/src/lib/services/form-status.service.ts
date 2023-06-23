14453905b5f8f9802270b75ead89fc7b5af34692
QianKai <eqiankai@hotmail.com>
2023-06-23 12:42:14 +0800

    refactor(lib): reduce cognitive complexity

--- projects/ng-dynamic-json-form/src/lib/services/form-status.service.ts
+++ projects/ng-dynamic-json-form/src/lib/services/form-status.service.ts
@@ -4,8 +4,7 @@ import {
   FormGroup,
   ValidationErrors,
   isFormArray,
-  isFormControl,
-  isFormGroup,
+  isFormGroup
 } from '@angular/forms';
 import {
   Observable,
@@ -83,10 +82,6 @@ export class FormStatusService {
     let result = prevResult ? { ...prevResult } : null;
     let errorsGet = null;
 
-    if (isFormControl(control)) {
-      errorsGet = controlErrors;
-    }
-
     if (isFormGroup(control)) {
       errorsGet = Object.keys(control.controls).reduce((acc, key) => {
         const err = this._getFormErrors(control.controls[key], result);
@@ -105,8 +100,9 @@ export class FormStatusService {
       };
     }
 
+    errorsGet = controlErrors;
     result = clearEmpties({ ...result, ...errorsGet });
-    
+
     const noErrors = !result || !Object.keys(result).length;
     return noErrors ? null : result;
   }
@@ -117,40 +113,43 @@ export class FormStatusService {
   ): void {
     const conditions = data.conditions;
     const control = data.targetControl;
+    const validatorsAndConditions = Object.values(ValidatorAndConditionTypes);
 
-    const result = (type: string) => {
-      // Pick only first level condition type, to prevent complexity goes up
-      const targetConditions = conditions.filter((x) => x.name === type);
-      if (!targetConditions.length) return undefined;
+    const toggleHiddenState = (hidden: boolean) => {
+      this._targetElement$(data.targetControlPath).then((x) => {
+        if (!x) return;
 
-      return this._getConditionResult(form, targetConditions);
+        this._setElementStyle(x, 'display', hidden ? 'none' : 'block');
+        hidden ? control.disable() : control.enable();
+      });
     };
 
-    const setStatus = (type: string) => {
-      const bool = result(type);
-      if (bool === undefined) return;
+    for (const key of validatorsAndConditions) {
+      // Pick only first level condition type, to prevent complexity goes up
+      const targetConditions = conditions.filter((x) => x.name === key);
+      const bool = !targetConditions.length
+        ? undefined
+        : this._getConditionResult(form, targetConditions);
 
-      switch (type) {
+      if (bool === undefined) {
+        return;
+      }
+
+      switch (key) {
         case ValidatorAndConditionTypes.HIDDEN:
-          this._targetElement$(data.targetControlPath).then((x) => {
-            if (!x) return;
-            this._setElementStyle(x, 'display', bool ? 'none' : 'block');
-            bool ? control.disable() : control.enable();
-          });
+          toggleHiddenState(bool);
           break;
 
         case ValidatorAndConditionTypes.DISABLED:
-          if (bool) control.disable();
-          else control.enable();
+          bool ? control.disable() : control.enable();
           break;
 
         default:
-          this._toggleValidators(control, bool, type, data.validators);
+          this._toggleValidators(control, bool, key, data.validators);
           break;
       }
-    };
+    }
 
-    Object.values(ValidatorAndConditionTypes).forEach((x) => setStatus(x));
     control.updateValueAndValidity();
   }
 
