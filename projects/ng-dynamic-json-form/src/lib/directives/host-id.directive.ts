e5f7b0a9b8038b049091c9f1fb134c062ecbb770
QianKai <eqiankai@hotmail.com>
2024-01-04 10:17:30 +0800

    chore: Refactor host-id directive

--- projects/ng-dynamic-json-form/src/lib/directives/host-id.directive.ts
+++ projects/ng-dynamic-json-form/src/lib/directives/host-id.directive.ts
@@ -8,20 +8,21 @@ export class HostIdDirective {
   private _renderer2 = inject(Renderer2);
   private _el = inject(ElementRef);
 
-  @Input() parentId = '';
-  @Input() controlName = '';
+  @Input() hostId = {
+    parentId: '',
+    controlName: '',
+  };
 
   ngOnChanges(): void {
     const hostEl = this._el.nativeElement as HTMLElement;
-    if (!hostEl) return;
+    if (!hostEl || !this.hostId.controlName) return;
 
     // Set `id` to this component so that `querySelector` can find it correctly.
     this._renderer2.setAttribute(hostEl, 'id', this._hostId);
   }
 
   private get _hostId(): string {
-    return this.parentId
-      ? `${this.parentId}.${this.controlName}`
-      : this.controlName;
+    const { parentId, controlName } = this.hostId;
+    return parentId ? `${parentId}.${controlName}` : controlName;
   }
 }
