a34cd47e0d703a1f4f8020b1084a8c0f8ece1351
QianKai <eqiankai@hotmail.com>
2025-07-29 19:09:34 +0800

    fix(playground): adapt to the change of using signal

--- src/app/example/components/custom-error-message/custom-error-message.component.ts
+++ src/app/example/components/custom-error-message/custom-error-message.component.ts
@@ -1,14 +1,13 @@
-
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { AbstractControl, FormControl } from '@angular/forms';
 import { CustomErrorMessage } from 'ng-dynamic-json-form';
 
 @Component({
-    selector: 'app-custom-error-message',
-    imports: [],
-    templateUrl: './custom-error-message.component.html'
+  selector: 'app-custom-error-message',
+  imports: [],
+  templateUrl: './custom-error-message.component.html',
 })
 export class CustomErrorMessageComponent implements CustomErrorMessage {
   control: AbstractControl<any, any> = new FormControl();
-  errorMessages: string[] = [];
+  errorMessages = signal<string[]>([]);
 }
